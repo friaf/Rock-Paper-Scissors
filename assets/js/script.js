1db8d96efb191d321fbd4d6f7137948de6d48daa
friaf <shovel.irena@gmail.com>
2022-09-26 01:07:37 +0000

    Add score count and messages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function() {
 
 
-    let messages = document.querySelector('#message');
+    let messages = document.querySelector('.comments');
     let computerScore = document.querySelector('#computer-score');
     let userScrore = document.querySelector('#user-score');
     let computerField = document.querySelector('.computer-field');
@@ -17,6 +17,11 @@ window.addEventListener('load', function() {
     let countUser = 0;
     let countComputer = 0;
     let blocked = false;
+
+
+    const winSound = new Audio('assets/audio/win.wav');
+    const pcSound = new Audio('assets/audio/pc.wav');
+    const drawSound = new Audio('assets/audio/draw.wav');
 
 
     function userGame(e){
@@ -36,12 +41,48 @@ window.addEventListener('load', function() {
       blocked = true;
       let randomChoice = Math.floor(Math.random() * 3)
       computerField.classList.add('blink');
-      
+      let computerFields = computerField.querySelectorAll('.field');
+
+      setTimeout(() => {
+        computerField.classList.remove('blink');
+        computerStep = computerFields[randomChoice].dataset.field;
+        computerFields[randomChoice].classList.add('active');
+        blocked=false;
+        winner()
+      }, 4000)
 
     }
 
     function winner (){
+        let both = userStep + computerStep;
+        switch (both) {
+            case 'rr':
+            case 'ss':
+            case 'pp':
+                messages.innerText = 'Its a draw!';
+                winSound.play();
+                break;   
+                
+                case 'rs':
+                case 'sp':
+                case 'pr':
+                    messages.innerText = 'You win!';
+                    winSound.play();
+                    countUser++;
+                    userScrore.innerText = countUser;
+                    break;  
 
+                    case 'sr':
+                    case 'ps':
+                    case 'rp':
+                        messages.innerText = 'Computer wins!';
+                        winSound.play();
+                        countComputer++;
+                        computerScore.innerText = countComputer;
+                        break;   
+                
+                
+        }
     }
 
     function playGame(){
